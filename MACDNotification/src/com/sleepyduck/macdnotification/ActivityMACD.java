5295a8bf155e6585c096dcd3fad47cb35f4dfe6c
Fredrik Metcalf <metcalf.fredrik@gmail.com>
2013-11-15 23:04:33 +0100

    Added possiblity to add groups
    Removed bin and gen from git

--- MACDNotification/src/com/sleepyduck/macdnotification/ActivityMACD.java
+++ MACDNotification/src/com/sleepyduck/macdnotification/ActivityMACD.java
@@ -1,5 +1,11 @@
 package com.sleepyduck.macdnotification;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,105 +14,147 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class ActivityMACD extends Activity {
-    public static final String KEY_INDEX_SYMBOLS = "Indexes";
-    public static final String KEY_STOCK_SYMBOLS = "Stocks";
-    public static final String KEY_ETF_SYMBOLS = "ETFs";
+	public static final String KEY_GROUP = "group";
+	public static final String KEY_COUNT = "count";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_SEPPARATOR = ":";
 
-    private List<String> mGroups;
-    private Map<String, List<String>> mSymbols;
-    private ExpandableListAdapter mListAdapter;
-    private Spinner mGroupSpinner;
+	private final List<String> mGroups = Collections.synchronizedList(new ArrayList<String>());
+	private final Map<String, List<String>> mSymbols = Collections
+			.synchronizedMap(new LinkedHashMap<String, List<String>>());
+	private ExpandableListAdapter mListAdapter;
+	private Spinner mGroupSpinner;
+	private ArrayAdapter<String> mSpinnerAdapter;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_macd);
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_macd);
 
-        mGroupSpinner = (Spinner) findViewById(R.id.spinnerGroup);
-        ExpandableListView mListView = (ExpandableListView) findViewById(R.id.listView);
+		mGroupSpinner = (Spinner) findViewById(R.id.spinnerGroup);
+		final ExpandableListView mListView = (ExpandableListView) findViewById(R.id.listView);
 
-        setupSpiner();
-        createGroupList();
-        loadSymbols();
+		mSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mGroups);
+		mGroupSpinner.setAdapter(mSpinnerAdapter);
 
-        mListAdapter = new ExpandableListAdapter(this, mGroups, mSymbols);
-        mListView.setAdapter(mListAdapter);
+		mListAdapter = new ExpandableListAdapter(this, mGroups, mSymbols);
+		mListView.setAdapter(mListAdapter);
 
-        mListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+		mListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
 
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-                final String symbol = (String) mListAdapter.getChild(
-                        groupPosition, childPosition);
-                new CalculateMACD(getApplicationContext()).execute(symbol, CalculateMACD.TOAST);
-                return true;
-            }
-        });
-    }
+			@Override
+			public boolean onChildClick(final ExpandableListView parent, final View v, final int groupPosition, final int childPosition, final long id) {
+				final String symbol = (String) mListAdapter.getChild(groupPosition, childPosition);
+				new CalculateMACD(getApplicationContext()).execute(symbol, CalculateMACD.TOAST);
+				return true;
+			}
+		});
+	}
 
-    private void setupSpiner() {
-        SpinnerAdapter spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
-                new String[] {KEY_INDEX_SYMBOLS, KEY_STOCK_SYMBOLS, KEY_ETF_SYMBOLS});
-        mGroupSpinner.setAdapter(spinnerAdapter);
-    }
+	public void onAddSymbolClicked(final View view) {
+		final View addLayout = findViewById(R.id.addLayout);
+		addLayout.setVisibility(View.GONE);
+		if (mGroupSpinner.getVisibility() == View.VISIBLE) {
+			final String group = (String) mGroupSpinner.getSelectedItem();
+			if (group != null) {
+				final List<String> symbols = mSymbols.get(group);
+				final EditText symbolText = (EditText) findViewById(R.id.editTextNewSymbol);
+				if (symbolText != null && symbolText.getText() != null
+						&& !symbolText.getText().toString().equals(""))
+					symbols.add(symbolText.getText().toString());
+			}
+		} else {
+			final EditText groupText = (EditText) findViewById(R.id.editTextNewSymbol);
+			if (groupText != null && groupText.getText() != null
+					&& !groupText.getText().toString().equals("")) {
+				mGroups.add(groupText.getText().toString());
+				mSymbols.put(groupText.getText().toString(), new ArrayList<String>());
+			}
+		}
+		mListAdapter.notifyDataSetChanged();
+		mSpinnerAdapter.notifyDataSetChanged();
+	}
 
-    private void loadSymbols() {
-        SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
-        if (mSymbols == null)
-            mSymbols = new LinkedHashMap<String, List<String>>();
-        Set<String> symbols = prefs.getStringSet(KEY_INDEX_SYMBOLS, new HashSet<String>());
-        mSymbols.put(mGroups.get(0), new ArrayList<String>(symbols));
-        symbols = prefs.getStringSet(KEY_STOCK_SYMBOLS, new HashSet<String>());
-        mSymbols.put(mGroups.get(1), new ArrayList<String>(symbols));
-        symbols = prefs.getStringSet(KEY_ETF_SYMBOLS, new HashSet<String>());
-        mSymbols.put(mGroups.get(2), new ArrayList<String>(symbols));
-    }
+	public void onNewGroupClicked(final View view) {
+		final View addLayout = findViewById(R.id.addLayout);
+		if (mGroupSpinner.getVisibility() == View.VISIBLE || addLayout.getVisibility() == View.GONE) {
+			final EditText editText = (EditText) findViewById(R.id.editTextNewSymbol);
+			mGroupSpinner.setVisibility(View.GONE);
+			if (editText != null) {
+				editText.setText("");
+				editText.setHint(R.string.group_name);
+			}
+			addLayout.setVisibility(View.VISIBLE);
+		} else {
+			addLayout.setVisibility(View.GONE);
+		}
+	}
 
-    private void createGroupList() {
-        mGroups = new ArrayList<String>();
-        mGroups.add(KEY_INDEX_SYMBOLS);
-        mGroups.add(KEY_STOCK_SYMBOLS);
-        mGroups.add(KEY_ETF_SYMBOLS);
-    }
+	public void onNewSymbolClicked(final View view) {
+		final View addLayout = findViewById(R.id.addLayout);
+		if (mGroupSpinner.getVisibility() == View.GONE || addLayout.getVisibility() == View.GONE) {
+			mGroupSpinner.setVisibility(View.VISIBLE);
+			final EditText editText = (EditText) findViewById(R.id.editTextNewSymbol);
+			if (editText != null) {
+				editText.setText("");
+				editText.setHint(R.string.symbol_name);
+			}
+			addLayout.setVisibility(View.VISIBLE);
+		} else {
+			addLayout.setVisibility(View.GONE);
+		}
+	}
 
-    public void onResume() {
-        super.onResume();
-        loadSymbols();
-        mListAdapter.notifyDataSetChanged();
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		final SharedPreferences.Editor editor = getSharedPreferences(getPackageName(), MODE_PRIVATE).edit();
+		editor.clear();
+		String groupName;
+		editor.putInt(KEY_COUNT, mGroups.size());
+		List<String> symbols;
+		for (int i = 0; i < mGroups.size(); i++) {
+			groupName = mGroups.get(i);
+			if (groupName.length() > 0) {
+				editor.putString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + KEY_NAME, groupName);
+				symbols = mSymbols.get(groupName);
+				editor.putInt(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + KEY_COUNT, symbols.size());
+				Collections.sort(symbols);
+				for (int j = 0; j < symbols.size(); j++) {
+					if (symbols.get(j).length() > 0) {
+						editor.putString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_NAME,
+								symbols.get(j));
+					}
+				}
+			}
+		}
+		editor.commit();
+	}
 
-    public void onPause(){
-        super.onPause();
-        SharedPreferences.Editor editor = getSharedPreferences(getPackageName(), MODE_PRIVATE).edit();
-        editor.putStringSet(KEY_INDEX_SYMBOLS, new HashSet<String>(mSymbols.get(mGroups.get(0))));
-        editor.putStringSet(KEY_STOCK_SYMBOLS, new HashSet<String>(mSymbols.get(mGroups.get(1))));
-        editor.putStringSet(KEY_ETF_SYMBOLS, new HashSet<String>(mSymbols.get(mGroups.get(2))));
-        editor.commit();
-    }
-
-    public void onNewSymbolClicked(View view) {
-        View addLayout = findViewById(R.id.addLayout);
-        addLayout.setVisibility(View.VISIBLE);
-    }
-
-    public void onAddSymbolClicked(View view) {
-        View addLayout = findViewById(R.id.addLayout);
-        addLayout.setVisibility(View.GONE);
-        String group = (String) mGroupSpinner.getSelectedItem();
-        List<String> symbols = mSymbols.get(group);
-        EditText symbolText = (EditText) findViewById(R.id.editTextNewSymbol);
-        if (symbolText != null && symbolText.getText() != null)
-            symbols.add(symbolText.getText().toString());
-        mListAdapter.notifyDataSetChanged();
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		mGroups.clear();
+		mSymbols.clear();
+		final SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+		int symbolCount;
+		String groupName;
+		List<String> symbols;
+		final int groupCount = prefs.getInt(KEY_COUNT, 0);
+		for (int i = 0; i < groupCount; i++) {
+			groupName = prefs.getString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + KEY_NAME, "Group");
+			mGroups.add(groupName);
+			symbols = new ArrayList<String>();
+			mSymbols.put(groupName, symbols);
+			symbolCount = prefs.getInt(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + KEY_COUNT, 0);
+			for (int j = 0; j < symbolCount; j++) {
+				symbols.add(prefs.getString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_NAME,
+						"-SYM-"));
+			}
+		}
+		mListAdapter.notifyDataSetChanged();
+		mSpinnerAdapter.notifyDataSetChanged();
+	}
 }
--- MACDNotification/src/com/sleepyduck/macdnotification/StartupBroadcastReceiver.java
+++ MACDNotification/src/com/sleepyduck/macdnotification/StartupBroadcastReceiver.java
@@ -1,8 +1,8 @@
 package com.sleepyduck.macdnotification;
 
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -37,11 +37,21 @@ public class StartupBroadcastReceiver extends BroadcastReceiver {
 		alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
 				PendingIntent.getBroadcast(context, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
 
-        SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(),
-                Context.MODE_PRIVATE);
-		final Set<String> symbols = prefs.getStringSet(ActivityMACD.KEY_INDEX_SYMBOLS, new HashSet<String>());
-        symbols.addAll(prefs.getStringSet(ActivityMACD.KEY_STOCK_SYMBOLS, new HashSet<String>()));
-        symbols.addAll(prefs.getStringSet(ActivityMACD.KEY_ETF_SYMBOLS, new HashSet<String>()));
+		final SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(),
+				Context.MODE_PRIVATE);
+
+		int symbolCount;
+		final List<String> symbols = new ArrayList<String>();
+		final int groupCount = prefs.getInt(ActivityMACD.KEY_COUNT, 0);
+		for (int i = 0; i < groupCount; i++) {
+			symbolCount = prefs.getInt(ActivityMACD.KEY_GROUP + ActivityMACD.KEY_SEPPARATOR + i
+					+ ActivityMACD.KEY_SEPPARATOR + ActivityMACD.KEY_COUNT, 0);
+			for (int j = 0; j < symbolCount; j++) {
+				symbols.add(prefs.getString(ActivityMACD.KEY_GROUP + ActivityMACD.KEY_SEPPARATOR + i
+						+ ActivityMACD.KEY_SEPPARATOR + j + ActivityMACD.KEY_NAME, "-SYM-"));
+			}
+		}
+
 		for (final String symbol : symbols)
 			new CalculateMACD(context).execute(symbol, CalculateMACD.NOTIFICATION);
 	}
