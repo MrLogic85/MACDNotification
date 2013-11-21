06d46c34057b46301f6d26d023f1f098e3458964
Fredrik Metcalf <metcalf.fredrik@gmail.com>
2013-11-21 23:33:27 +0100

    Saving to file
    Need to save to external storage, else you cannot store a backup file.

--- MACDNotification/src/com/sleepyduck/macdnotification/ActivityMACD.java
+++ MACDNotification/src/com/sleepyduck/macdnotification/ActivityMACD.java
@@ -1,10 +1,10 @@
 package com.sleepyduck.macdnotification;
 
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
@@ -17,20 +17,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ActivityMACD extends Activity {
 	public static final String KEY_GROUP = "group";
 	public static final String KEY_COUNT = "count";
 	public static final String KEY_NAME = "name";
 	public static final String KEY_SEPPARATOR = ":";
-    public static final String ACTION_BROADCAST_REMOVE = "ActivityMACD:action_broadcast_remove";
-    private static final String KEY_VALUE_SEPPARATOR = "<=>";
+	public static final String ACTION_BROADCAST_REMOVE = "ActivityMACD:action_broadcast_remove";
+	private static final String KEY_VALUE_SEPPARATOR = "<=>";
 
 	private final List<String> mGroups = Collections.synchronizedList(new ArrayList<String>());
 	private final Map<String, List<String>> mSymbols = Collections
@@ -38,44 +40,44 @@ public class ActivityMACD extends Activity {
 	private ExpandableListAdapter mListAdapter;
 	private Spinner mGroupSpinner;
 	private ArrayAdapter<String> mSpinnerAdapter;
-    private View mAddLayout;
-    private EditText mNameEditText;
+	private View mAddLayout;
+	private EditText mNameEditText;
 
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.hasExtra(KEY_GROUP) && intent.hasExtra(KEY_NAME)) {
-                String group = intent.getStringExtra(KEY_GROUP);
-                String symbol = intent.getStringExtra(KEY_NAME);
-                if (mGroupSpinner.getVisibility() == View.GONE || mAddLayout.getVisibility() == View.GONE) {
-                    onNewSymbolClicked(null);
-                }
-                if (mNameEditText != null) {
-                    mNameEditText.setText(symbol);
-                    mNameEditText.requestFocus();
-                }
-                mGroupSpinner.setSelection(mGroups.indexOf(group));
-            }
-        }
-    };
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.hasExtra(KEY_GROUP) && intent.hasExtra(KEY_NAME)) {
+				String group = intent.getStringExtra(KEY_GROUP);
+				String symbol = intent.getStringExtra(KEY_NAME);
+				if (mGroupSpinner.getVisibility() == View.GONE || mAddLayout.getVisibility() == View.GONE) {
+					onNewSymbolClicked(null);
+				}
+				if (mNameEditText != null) {
+					mNameEditText.setText(symbol);
+					mNameEditText.requestFocus();
+				}
+				mGroupSpinner.setSelection(mGroups.indexOf(group));
+			}
+		}
+	};
 
-    private ExpandableListView.OnChildClickListener mChildClickListener = new ExpandableListView.OnChildClickListener() {
-        @Override
-        public boolean onChildClick(final ExpandableListView parent, final View v, final int groupPosition, final int childPosition, final long id) {
-            final String symbol = (String) mListAdapter.getChild(groupPosition, childPosition);
-            new CalculateMACD(getApplicationContext()).execute(symbol, CalculateMACD.TOAST);
-            return true;
-        }
-    };
+	private ExpandableListView.OnChildClickListener mChildClickListener = new ExpandableListView.OnChildClickListener() {
+		@Override
+		public boolean onChildClick(final ExpandableListView parent, final View v, final int groupPosition, final int childPosition, final long id) {
+			final String symbol = (String) mListAdapter.getChild(groupPosition, childPosition);
+			new CalculateMACD(getApplicationContext()).execute(symbol, CalculateMACD.TOAST);
+			return true;
+		}
+	};
 
-    @Override
+	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_macd);
 
 		mGroupSpinner = (Spinner) findViewById(R.id.spinnerGroup);
-        mAddLayout = findViewById(R.id.addLayout);
-        mNameEditText = (EditText) findViewById(R.id.editTextNewSymbol);
+		mAddLayout = findViewById(R.id.addLayout);
+		mNameEditText = (EditText) findViewById(R.id.editTextNewSymbol);
 
 		final ExpandableListView mListView = (ExpandableListView) findViewById(R.id.listView);
 
@@ -86,6 +88,42 @@ public class ActivityMACD extends Activity {
 		mListView.setAdapter(mListAdapter);
 
 		mListView.setOnChildClickListener(mChildClickListener);
+
+		load();
+	}
+
+	private void load() {
+		Editor prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE).edit();
+		prefs.clear();
+		try {
+			InputStreamReader in = new InputStreamReader(openFileInput("symbols.data"));
+			BufferedReader reader = new BufferedReader(in);
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] keyVal = line.split(KEY_VALUE_SEPPARATOR);
+				if (keyVal.length > 1) {
+					Toast.makeText(this, "KeyVal: " + keyVal[0] + " = " + keyVal[1], Toast.LENGTH_SHORT).show();
+					if (keyVal[0].contains(KEY_COUNT)) {
+						try {
+							prefs.putInt(keyVal[0], Integer.parseInt(keyVal[1]));
+						} catch (NumberFormatException e) {
+							e.printStackTrace();
+						}
+					} else {
+						prefs.putString(keyVal[0], keyVal[1]);
+					}
+				} else {
+					Toast.makeText(this, "KeyVal error: " + keyVal, Toast.LENGTH_SHORT).show();
+				}
+			}
+			prefs.commit();
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void onAddSymbolClicked(final View view) {
@@ -113,12 +151,12 @@ public class ActivityMACD extends Activity {
 		if (mGroupSpinner.getVisibility() == View.VISIBLE || mAddLayout.getVisibility() == View.GONE) {
 			mGroupSpinner.setVisibility(View.GONE);
 			if (mNameEditText != null) {
-                mNameEditText.setText("");
-                mNameEditText.setHint(R.string.group_name);
+				mNameEditText.setText("");
+				mNameEditText.setHint(R.string.group_name);
 			}
-            mAddLayout.setVisibility(View.VISIBLE);
+			mAddLayout.setVisibility(View.VISIBLE);
 		} else {
-            mAddLayout.setVisibility(View.GONE);
+			mAddLayout.setVisibility(View.GONE);
 		}
 	}
 
@@ -126,12 +164,12 @@ public class ActivityMACD extends Activity {
 		if (mGroupSpinner.getVisibility() == View.GONE || mAddLayout.getVisibility() == View.GONE) {
 			mGroupSpinner.setVisibility(View.VISIBLE);
 			if (mNameEditText != null) {
-                mNameEditText.setText("");
-                mNameEditText.setHint(R.string.symbol_name);
+				mNameEditText.setText("");
+				mNameEditText.setHint(R.string.symbol_name);
 			}
-            mAddLayout.setVisibility(View.VISIBLE);
+			mAddLayout.setVisibility(View.VISIBLE);
 		} else {
-            mNameEditText.setVisibility(View.GONE);
+			mNameEditText.setVisibility(View.GONE);
 		}
 	}
 
@@ -152,7 +190,7 @@ public class ActivityMACD extends Activity {
 				Collections.sort(symbols);
 				for (int j = 0; j < symbols.size(); j++) {
 					if (symbols.get(j).length() > 0) {
-						editor.putString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_NAME,
+						editor.putString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_SEPPARATOR + KEY_NAME,
 								symbols.get(j));
 					}
 				}
@@ -160,7 +198,7 @@ public class ActivityMACD extends Activity {
 		}
 		editor.commit();
 
-        registerReceiver(mReceiver, new IntentFilter(ACTION_BROADCAST_REMOVE));
+		unregisterReceiver(mReceiver);
 	}
 
 	@Override
@@ -180,41 +218,36 @@ public class ActivityMACD extends Activity {
 			mSymbols.put(groupName, symbols);
 			symbolCount = prefs.getInt(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + KEY_COUNT, 0);
 			for (int j = 0; j < symbolCount; j++) {
-				symbols.add(prefs.getString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_NAME,
-						"-SYM-"));
+				String name = prefs.getString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_NAME, "-SYM-");
+				if (name.equals("-SYM-"))
+					name = prefs.getString(KEY_GROUP + KEY_SEPPARATOR + i + KEY_SEPPARATOR + j + KEY_SEPPARATOR + KEY_NAME, "-SYM-");
+				symbols.add(name);
 			}
 		}
 		mListAdapter.notifyDataSetChanged();
 		mSpinnerAdapter.notifyDataSetChanged();
 
-        unregisterReceiver(mReceiver);
+		registerReceiver(mReceiver, new IntentFilter(ACTION_BROADCAST_REMOVE));
 	}
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
-        prefs.getAll();
-
-        File f = new File("symbols.data");
-        if (f != null && f.exists()) {
-            if (f.canWrite()) {
-                try {
-                    BufferedWriter out = new BufferedWriter(new FileWriter(f));
-                    String line;
-                    for (String key : prefs.getAll().keySet()) {
-                        Object val = prefs.getAll().get(key);
-                        line = key + KEY_VALUE_SEPPARATOR + val + "\n";
-                        out.write(line);
-                    }
-                    out.flush();
-                    out.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	@Override
+	public void onStop() {
+		super.onStop();
+		SharedPreferences prefs = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+		try {
+			OutputStreamWriter out = new OutputStreamWriter(openFileOutput("symbols.data", Context.MODE_PRIVATE));
+			String line;
+			for (String key : prefs.getAll().keySet()) {
+				Object val = prefs.getAll().get(key);
+				line = key + KEY_VALUE_SEPPARATOR + val + "\n";
+				out.write(line);
+			}
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
